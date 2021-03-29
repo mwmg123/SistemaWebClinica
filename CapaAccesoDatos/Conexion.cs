5fae61882f430e272f8ba4db5127f6d762518dbe
mwmg123 <mwmg2709@gmail.com>
2021-03-29 00:14:52 -0500

    Para ver si por esto salio pendiente de agregacion

---
+++ CapaAccesoDatos/Conexion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaAccesoDatos
+{
+    class Conexion
+    {
+        #region "PATRON SINGLETON"
+        private static Conexion conexion = null;
+        private Conexion ()
+        {
+
+        }
+        public static Conexion getInstance ()
+        {
+            if (conexion == null)
+            {
+                conexion = new Conexion();
+            }
+            return conexion;
+        }
+
+        #endregion
+
+        public SqlConnection ConexionBD()
+        {
+            SqlConnection conexion = new SqlConnection();
+            conexion.ConnectionString = "Data Source=.;Initial Catalog=DBClinica;User ID=sa;Password=79138524565";
+            return conexion;
+        }
+    }
+}
---
+++ CapaAccesoDatos/EmpleadoDAO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using CapaEntidades;
+
+namespace CapaAccesoDatos
+{
+    class EmpleadoDAO
+    {
+        #region "PATRON SINGLETON"
+        private static EmpleadoDAO daoEmpleado = null;
+        private EmpleadoDAO ()
+        {
+
+        }
+        public static EmpleadoDAO getInstance ()
+        {
+            if (daoEmpleado == null)
+            {
+                daoEmpleado = new EmpleadoDAO();
+            }
+            return daoEmpleado;
+        }
+        #endregion
+
+        public Empleado AccesoSistema(String user, String pass)
+        {
+            SqlConnection conexion = null;
+            SqlCommand cmd = null;
+            Empleado objEmpleado = null;
+            SqlDataReader dr = null;
+            try
+            {
+                conexion = Conexion.getInstance().ConexionBD();
+                cmd = new SqlCommand("spAccesoSistema", conexion);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
